87a897146b9c19e52796b9e061bf265f5757c0a4
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-03-13 09:32:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    if (num <= 0 || num > 100000) {
+        return "Invalid input"; // Handle out of range cases
+    }
+
+    const obj = {
+        0: ['M', 1000], 
+        1: ['CM', 900], 
+        2: ['D', 500], 
+        3: ['CD', 400], 
+        4: ['C', 100], 
+        5: ['XC', 90], 
+        6: ['L', 50], 
+        7: ['XL', 40], 
+        8: ['X', 10], 
+        9: ['IX', 9], 
+        10: ['V', 5], 
+        11: ['IV', 4], 
+        12: ['I', 1]
     };
 
-  //your code here
-	let result = '';
+    let result = '';
 
-  for (let i = 0; i < 7; i++) {
-    while (num >= obj[i][1]) {
-      result += obj[i][0]; 
-      num -= obj[i][1]; 
+    for (let i = 0; i < 13; i++) {
+        while (num >= obj[i][1]) {
+            result += obj[i][0]; 
+            num -= obj[i][1]; 
+        }
     }
-  }
 
-  return result;
-
+    return result;
 }
 
 
